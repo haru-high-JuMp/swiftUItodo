8e4b8a97ac1f06d0de6539c82db62b1e264fdb92
Haruki mori <haru0dev@gmail.com>
2023-07-26 18:09:59 +0900

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,20 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 森温起 on 2023/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
